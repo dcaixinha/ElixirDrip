dc08e2a13061082a744064cdc55a076ff25db776
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2017-12-16 19:10:57 +0000

    Better umbrella module name
    ElixirDrip.Umbrella instead of ElixirDripUmbrella.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ElixirDripUmbrella
+# ElixirDrip Umbrella
 
 **TODO: Add description**
 
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule ElixirDripUmbrella.Mixfile do
+defmodule ElixirDrip.Umbrella.Mixfile do
   use Mix.Project
 
   def project do
