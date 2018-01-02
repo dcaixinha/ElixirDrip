93955b873da4ede91dad4e3f321e1ef20f62bd62
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2018-01-02 08:42:15 +0000

    ElixirDrip.Storage context using a ElixirDrip.Storage.Provider
    The `ElixirDrip.Storage.Provider` delegates its `upload/2` and
    `download/1` calls to the module indicated by the `:storage_provider`
    configuration entry, which implements the
    `ElixirDrip.Behaviours.StorageProvider`.
    
    Currently, it delegates to `ElixirDrip.Storage.Providers.GoogleCloudStorage.Local`.

--- apps/elixir_drip/config/config.exs
+++ apps/elixir_drip/config/config.exs
@@ -2,4 +2,7 @@ use Mix.Config
 
 config :elixir_drip, ecto_repos: [ElixirDrip.Repo]
 
+config :elixir_drip,
+  storage_provider: ElixirDrip.Storage.Providers.GoogleCloudStorage.Local
+
 import_config "#{Mix.env()}.exs"
--- apps/elixir_drip/lib/elixir_drip/storage/media.ex
+++ apps/elixir_drip/lib/elixir_drip/storage/media.ex
@@ -3,9 +3,11 @@ defmodule ElixirDrip.Storage.Media do
 
   @primary_key {:id, ElixirDrip.Ecto.Ksuid, autogenerate: true}
   schema "storage_media" do
-    field :path, :string
-    field :encryption_key, :string
+    field :filename, :string
+    field :full_path, :string
     field :metadata, :map
+    field :encryption_key, :string
+    field :storage_key, :string
 
     timestamps()
   end
