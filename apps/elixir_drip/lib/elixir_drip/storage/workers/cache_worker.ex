c135f321406f5d7e7c9e42db29cb08493204f92d
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2018-01-12 19:07:25 +0000

    CacheWorker serves the content it receives on `init`
    It dies after @expire_time (currently 60 secs). It resets its expiration
    timer each time it serves a new `:get_media` request.

---
+++ apps/elixir_drip/lib/elixir_drip/storage/workers/cache_worker.ex
@@ -0,0 +1,35 @@
+defmodule ElixirDrip.Storage.Workers.CacheWorker do
+  use     GenServer
+  require Logger
+
+  @expire_time 60_000
+  def start_link(media_id, content) do
+    GenServer.start_link(__MODULE__, content, name: {:global, {:cache, media_id}})
+  end
+
+  def init(content) do
+    timer = Process.send_after(self(), :expire, @expire_time)
+    Logger.debug("#{inspect(self())}: CacheWorker started.  Will expire in #{Process.read_timer(timer)} milliseconds.")
+
+    {:ok, %{hits: 0, content: content, timer: timer}}
+  end
+
+  def handle_info(:expire, %{hits: hits} = state) do
+    Logger.debug("#{inspect(self())}: Terminating process... Served the cached content #{hits} times.")
+    {:stop, :normal, state}
+  end
+
+  def handle_info(msg, state) do
+    super(msg, state)
+  end
+
+  def handle_call(:get_media, _from, %{hits: hits, content: content, timer: timer} = state) do
+    Logger.debug("#{inspect(self())}: Received :get_media and served #{byte_size(content)} bytes #{hits+1} times.")
+
+    Process.cancel_timer(timer)
+    new_timer = Process.send_after(self(), :expire, @expire_time)
+    Logger.debug("#{inspect(self())}: Canceled the previous expiration timer. Will now expire in #{Process.read_timer(new_timer)} milliseconds.")
+
+    {:reply, content, %{state | hits: hits + 1, timer: new_timer}}
+  end
+end
