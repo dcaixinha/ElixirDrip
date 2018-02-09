0b70a41e7e63d35715e9b410e82dd898ecb2b2f7
André Albuquerque <andre.m.de.albuquerque@gmail.com>
2018-02-09 08:30:02 +0000

    WIP Flow work

--- apps/elixir_drip/lib/elixir_drip/storage/media.ex
+++ apps/elixir_drip/lib/elixir_drip/storage/media.ex
@@ -3,8 +3,10 @@ defmodule ElixirDrip.Storage.Media do
 
   @primary_key {:id, ElixirDrip.Ecto.Ksuid, autogenerate: true}
   schema "storage_media" do
+    field :user_id, ElixirDrip.Ecto.Ksuid
     field :file_name, :string
     field :full_path, :string
+    field :file_size, :integer
     field :metadata, :map, default: %{}
     field :encryption_key, :string
     field :storage_key, :string
