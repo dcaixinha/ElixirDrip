c7879773e4c8959c8f1dd07c32a2f77ef81a3e77
André Albuquerque <andre.albuquerque@onfido.com>
2018-05-12 06:58:36 +0100

    Updates to `ecto.exs` and new `ecto_queries.exs` example

--- examples/ecto.exs
+++ examples/ecto.exs
@@ -4,8 +4,12 @@ u = %{username: "ana", password: "qwerasdf", email: "ana@right.there"}
 u = %{username: "jose", password: "qwerasdf", email: "jose@hey.ho"}
 {:ok, user} = ElixirDrip.Accounts.create_user(u)
 
+u = %{username: "andre", password: "qwerasdf", email: "andre@somewhere.com"}
+{:ok, user} = ElixirDrip.Accounts.create_user(u)
+
 alias ElixirDrip.Repo
 alias ElixirDrip.Accounts
+alias ElixirDrip.Accounts.User
 alias ElixirDrip.Storage
 alias ElixirDrip.Storage.Media
 alias ElixirDrip.Storage.Owner
@@ -29,13 +33,17 @@ jose_user = Accounts.get_user_by_username("jose")
 jose_owner = Storage.get_owner(jose_user.id)
 jose_media = Storage.get_all_media(jose_owner.id)
 
+Storage.store(owner.id, "hello_world.txt", "$/hello/world", "Hi there, this is a test.")
 Storage.store(jose_owner.id, "111.txt", "$/first/folder", "content content 111")
 Storage.store(jose_owner.id, "222.txt", "$/second/folder", "content content 222")
 Storage.store(jose_owner.id, "333.txt", "$/third/folder", "content content 333")
 Storage.store(jose_owner.id, "README.md", "$", "README content")
 Storage.store(jose_owner.id, "CHANGELOG.md", "$", "CHANGELOG content")
 Storage.store(jose_owner.id, "howto.md", "$/first", "howto content")
+Storage.store(jose_owner.id, "small1.md", "$/small_files", "1")
+Storage.store(jose_owner.id, "small2.md", "$/small_files", "2")
 
+Repo.query("select * from pg_catalog.pg_tables")
 Repo.query("select * from media_owners")
 Repo.query("select exists(select * from storage_media where full_path = '$' and file_name = 'README.md')")
 Repo.query("select RIGHT('#{path}', -#{String.length(pwd)})")
@@ -56,3 +64,11 @@ last_media_id = last_media[:id]
 Storage.move(jose_owner.id, last_media_id, "$/qaz")
 Storage.move(jose_owner.id, readme.id, "$")
 Storage.rename(jose_owner.id, readme.id, "README.md")
+
+Storage.get_all_media(jose_owner.id) |> Enum.at(-1) |> Ecto.Changeset.cast(%{full_path: "$/as/"}, [:full_path]) |> Media.validate_field(:full_path)
+
+owner = jose_owner
+file_name = "example.md"
+full_path = "$/an/example/folder"
+file_size = 123456
+Media.create_initial_changeset(owner.id, file_name, full_path, file_size)
