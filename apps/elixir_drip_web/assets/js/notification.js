303397168333f329760ee4c219991b2eb89a5bea
Daniel Caixinha <2683004+dcaixinha@users.noreply.github.com>
2018-05-17 05:01:53 +0100

    Stuff to put Share of files working

--- apps/elixir_drip_web/assets/js/notification.js
+++ apps/elixir_drip_web/assets/js/notification.js
@@ -4,15 +4,21 @@ let Notification = {
     let successArea = document.getElementById("notify_success")
     let userChannel = socket.channel("users:" + userId)
 
+    let share_button = document.getElementById("share_file_btn")
+    if (share_button != null) {
+      share_button.onclick = () => { this.sharePopUp(userChannel) }
+    }
+
     userChannel.join()
       .receive("ok", resp => { console.log("Joined successfully", resp) })
       .receive("error", reason => { console.log("Unable to join", reason) })
 
-    userChannel.on("upload", ({message}) => this.renderUploadNotifcation(infoArea, successArea, message))
+    userChannel.on("upload", ({message}) => this.renderNotifcation(infoArea, successArea, message))
     userChannel.on("download", ({message, link}) => this.renderDownloadNotifcation(infoArea, successArea, message, link))
+    userChannel.on("share", ({message}) => this.renderNotifcation(infoArea, successArea, message))
   },
 
-  renderUploadNotifcation(infoArea, successArea, message) {
+  renderNotifcation(infoArea, successArea, message) {
     infoArea.innerHTML = ""
     successArea.innerHTML = message
   },
@@ -20,6 +26,16 @@ let Notification = {
   renderDownloadNotifcation(infoArea, successArea, message, link) {
     infoArea.innerHTML = ""
     successArea.innerHTML = `<a href="${link}">${message}</a>`
+  },
+
+  sharePopUp(userChannel) {
+    let username = prompt("Share file with:", "username")
+    if (username != null && username != "") {
+      let splitUrl = window.location.href.split('/')
+      let fileId = splitUrl.pop() || splitUrl.pop()
+
+      userChannel.push("share", {username: username, file_id: fileId})
+    }
   }
 }
 
