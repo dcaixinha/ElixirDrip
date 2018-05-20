5e6887baaa23307ce7bb42057ef250769f3e17bd
André Albuquerque <andre.albuquerque@onfido.com>
2018-05-20 06:43:43 +0100

    K8s Prod deployment template and update Phoenix default view
    Currently the Deployment template still has the Docker image tag
    hardcoded, this will need to change to cater for the Continuous
    deployment via Travis CI.
    
    We pass the secrets to the Google Cloud SQL Proxy sidecar that we
    created via CLI.

--- apps/elixir_drip_web/test/elixir_drip_web/controllers/page_controller_test.exs
+++ apps/elixir_drip_web/test/elixir_drip_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ElixirDripWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to ElixirDrip v0.0.8!"
   end
 end
